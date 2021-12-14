b53ced7879c6c5a4d3a9c7c36267cf5afc0836ba
Sam Davenport <samueldavenport@att.net>
2021-12-14 08:46:24 -0600

    It is now possible to update and delete a category by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -61,12 +61,46 @@ router.post('/', (req, res) => {
   });
 });
 
+// update a category by its `id` value
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  // expects { category_name: 'Pants'}
+  Category.update(req.body, {
+    individualHooks: true,
+    where: {
+        id: req.params.id 
+    }
+})
+    .then(dbCategoryData => {
+        if (!dbCategoryData[0]) {
+            res.status(404).json({ message: 'No category found with this id'});
+            return;
+        }
+        res.json(dbCategoryData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
+// delete a category by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+        id: req.params.id 
+    },
+})
+    .then(dbCategoryData => {
+        if (!dbCategoryData) {
+            res.status(404).json({ message: 'No category found with this id'});
+            return;
+        }
+        res.json(dbCategoryData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
