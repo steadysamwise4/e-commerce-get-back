840cc2b680dd94387be686a98ef025d64d161c6c
Sam Davenport <samueldavenport@att.net>
2021-12-14 06:23:03 -0600

    get one product route now exists and functions

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -32,6 +32,27 @@ router.get('/', (req, res) => {
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  Product.findOne({
+    attributes: ['id', 'product_name', 'price', 'stock', ],
+    where: { id: req.params.id },
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name', 'id']
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name'],
+        through: ProductTag,
+        as: 'tagged_products'
+      }
+    ]
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
   // be sure to include its associated Category and Tag data
 });
 
