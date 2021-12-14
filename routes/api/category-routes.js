e6a107ac040a95efd9205287bb118a90365efd01
Sam Davenport <samueldavenport@att.net>
2021-12-14 08:32:50 -0600

    routes to get one category and create a category added and tested

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -26,11 +26,39 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  Category.findOne({
+    attributes: ['id', 'category_name'],
+    where: { id: req.params.id },
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock']
+      }
+    ]
+  })
+  .then(dbCategoryData => {
+    if (!dbCategoryData) {
+      res.status(404).json({ message: 'No category found with this id' });
+      return;
+  }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+});
   // be sure to include its associated Products
 });
 
+// create a new category
 router.post('/', (req, res) => {
-  // create a new category
+  // expects { category_name: 'Pants'}
+  Category.create({ category_name: req.body.category_name })
+  .then(dbUserData => res.json(dbUserData))
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
